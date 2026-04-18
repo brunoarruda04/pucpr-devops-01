8f207540db75122181305654b96b0c4ed1970cf2
bruno <bruno_arruda_ba@hotmail.com>
2026-04-18 14:36:31 -0300

    chore: semantic release and broking ci to test

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ source 'https://rubygems.org'
 
 gem 'rubocop', require: false
 gem 'sinatra'
+
+gem "rackup", "~> 2.3"
+gem "puma", "~> 8.0"
--- app.rb
+++ app.rb
@@ -1,40 +1,38 @@
-# frozen_string_literal: true
-
 require 'sinatra'
 require 'json'
 
-helpers do
-  def tasks
-    settings.tasks ||= []
-  end
-end
+# armazenamento em memória
+set :tasks, []
 
 before do
   content_type :json
 end
 
 get '/' do
-  { message: 'API Ruby funcionando!' }.to_json
+  { message: "API Ruby funcionando!" }.to_json
 end
 
+# listar tarefas
 get '/tasks' do
-  tasks.to_json
+  settings.tasks.to_json
 end
 
+# criar tarefa
 post '/tasks' do
   data = JSON.parse(request.body.read)
 
   task = {
-    id: (tasks.size + 1).to_s,
-    title: data['title']
+    id: (settings.tasks.size + 1).to_s,
+    title: data["title"]
   }
 
-  tasks << task
+  settings.tasks << task
   task.to_json
 end
 
+# deletar tarefa
 delete '/tasks/:id' do
-  tasks.delete_if { |task| task[:id] == params[:id] }
+  settings.tasks.delete_if { |task| task[:id] == params[:id] }
   status 204
 end
 
